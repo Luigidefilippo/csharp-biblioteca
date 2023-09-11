065ed19564b803476da24469c41a5064bda5dd93
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-11 18:08:39 +0200

    print same information

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿namespace csharp_biblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book LeCronacheDiNarnia = new Book("Clean Code ", "01-08-2008", "Informatic", "Books-B16", "Robert C. Martin", 869);
+            Dvd SuperMarioBros = new Dvd("Batman - Il cavaliere oscuro ", "12-09-2008", "Action", "Dvds-D324", "Sony", 2.32);
+
+            List<Document> NewDocument = new List<Document>();
+            NewDocument.Add(LeCronacheDiNarnia);
+            NewDocument.Add(SuperMarioBros);
+
+            foreach (Document document in NewDocument)
+            {
+                document.PrintInfo();
+            }
+        }
+    }
+}
